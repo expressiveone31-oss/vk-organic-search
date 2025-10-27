ec8300fbb6ed7fed9460767fe7039f309896d993
expressiveone31-oss <expressive.one31@gmail.com>
2025-10-27 06:48:59 +0300

    Delete bot directory

--- bot/keyboards/common.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-def cancel_kb():
-    return ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="Отмена")]], resize_keyboard=True, one_time_keyboard=True)
